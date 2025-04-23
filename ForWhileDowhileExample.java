eb7db0a41bce3a23d83ada0aa8ca700a1daa8028
SumedhaBedradi <144426153+SumedhaBedradi@users.noreply.github.com>
2025-04-23 23:39:20 +0530

    Add files via upload

---
+++ ForWhileDowhileExample.java
@@ -0,0 +1,30 @@
+package JAVA_Practice;
+
+public class ForWhileDowhileExample {
+
+	public static void main(String[] args) {
+		// for loop
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+		System.out.println("_________________________________");
+
+		// while loop
+		int j = 1;
+		while (j <= 5) {
+			System.out.println(j);
+			j++;
+
+		}
+		System.out.println("___________________________________");
+
+		// do-while loop
+		int k = 1;
+		do {
+			System.out.println(k);
+			k++;
+		} while (k <= 5);
+
+	}
+
+}
