8465dc66df3cc8c44e2a97fbeb4ca51c3292f3e7
SumedhaBedradi <144426153+SumedhaBedradi@users.noreply.github.com>
2025-04-24 22:29:03 +0530

    Add files via upload

---
+++ weekendWeekday.java
@@ -0,0 +1,21 @@
+package JAVA_Practice;
+
+import java.util.Scanner;
+
+public class weekendWeekday {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if (num >= 1 && num <= 5) {
+			System.out.println("Weekday");
+		} else if (num == 6 || num == 7) {
+			System.out.println("Weekend");
+		} else {
+			System.out.println("Invalid input");
+		}
+
+	}
+
+}
