da74848c80aa6cf23e83c0e6a42c255673eb0ed0
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-05-07 18:33:47 +0530

    Mathematic operation without using reference variable

---
+++ MathematicOperation.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class additionSubtraction {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter operation to perform: ");
+		String operation = sc.next();
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		switch (operation) {
+		case "Addition":
+			MathOperation.additon(a, b);
+			break;
+		case "Subtraction":
+			System.out.println("Subtraction: " + MathOperation.subtraction(a, b));
+			break;
+		case "Multiplication":
+			MathOperation.multiplication(a, b);
+			break;
+		case "Division":
+			System.out.println("Division: " + MathOperation.division(a, b));
+			break;
+		}
+	}
+}
+
+class MathOperation {
+	static void additon(int a, int b) {
+		System.out.println("Addition: " + (a + b));
+	}
+
+	static int subtraction(int a, int b) {
+		return (a - b);
+	}
+
+	static void multiplication(int a, int b) {
+		System.out.println("Multiplication: " + (a * b));
+	}
+
+	static int division(int a, int b) {
+		return a / b;
+	}
+}
+
