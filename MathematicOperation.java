b54686d942ba57e9b1043a78d5e66535f790a9e7
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-05-08 21:11:12 +0530

    $*****\n*****\n*****\n*****

--- MathematicOperation.java
+++ MathematicOperation.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class additionSubtraction {
+public class MathematicOperation {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter operation to perform: ");
---
+++ pattern1.java
@@ -0,0 +1,10 @@
+public class pattern1 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {// i for row
+			for (int j = 1; j <= 5; j++) {// j for column
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
