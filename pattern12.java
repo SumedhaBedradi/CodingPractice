f5d0b79958e4b2a4ec40137145f3047652388b5f
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-05-19 19:52:38 +0530

    Pattern 12

---
+++ pattern12.java
@@ -0,0 +1,24 @@
+/*req. Output:
+ * i1j1
+i1j2
+i1j3
+i2j1
+i2j2
+i2j3
+i3j1
+i3j2
+i3j3
+ */
+public class pattern12{
+    public static void main(String[] args) {
+    int i = 1;
+    while (i < 4) {
+        int j = 1;
+        while (j < 4) {
+            System.out.println("i" + i + "j" + j);
+            j++;
+        }
+        i++;
+    }
+    }
+}
