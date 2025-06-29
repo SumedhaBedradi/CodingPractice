c24e4fb37b9b8214f51f224e7080e49081fb16a7
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-06-29 21:16:20 +0530

    Sum of elements in 2D array

---
+++ SumOfElementsIn2DArray.java
@@ -0,0 +1,21 @@
+public class SumOfElementsIn2DArray {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[2][3];
+//		Taking array data from user
+		System.out.println("Enter values to be stored in an array");
+		for (int i = 0; i <= arr.length - 1; i++) {
+			for (int j = 0; j <= arr[i].length - 1; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+//		Finding sum of elements in array
+		int sum = 0;
+		for (int i = 0; i <= arr.length - 1; i++) {
+			for (int j = 0; j <= arr[i].length - 1; j++) {
+				sum += arr[i][j];
+			}
+		}
+		System.out.println("Sum: " + sum);
+	}
+}
