29ec8dd2393f6b9a2807a55353c1d09717de3687
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-08-10 17:57:39 +0530

    First non-repeating character of given string

---
+++ FirstNonRepeatingCharacterOfGivenString.java
@@ -0,0 +1,24 @@
+public class FirstNonRepeatingCharacterOfGivenString {
+     public static char nonRep(String s) {
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            boolean found = false;
+
+            for (int j = 0; j < n; ++j) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) 
+                return s.charAt(i);
+        }
+
+        return '$'; // Return '$' if no non-repeating character is found.
+    }
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        System.out.println(nonRep(s));
+    }
+}
