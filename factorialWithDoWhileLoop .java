02f5e6c5b843ffadde53c1fd4d919f464f26ac6c
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-08-24 23:13:24 +0530

    Finding factorial of a number using do while loop

---
+++ factorialWithDoWhileLoop .java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class factorialWithDoWhileLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int factorial = 1;
+        int i = 1;
+        do {
+            factorial *= i;
+            i++;
+        } while (i <= number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+}   
