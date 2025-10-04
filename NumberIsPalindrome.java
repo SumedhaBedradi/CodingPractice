8d0102a26aefd00d3982e3bb3f359d0d6512ac7b
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-10-04 23:14:17 +0530

    Check number is palindrome or not

---
+++ NumberIsPalindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumberIsPalindrome {
+    public static void main(String args[]) {
+        System.out.print("Enter Number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r, sum = 0, temp;   
+        temp = n;
+
+        while(n > 0) {  
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+
+        if(temp == sum) 
+            System.out.println(temp + " is a Palindrome Number!");
+        else
+            System.out.println(temp + " is not a Palindrome Number!");
+    }
+}
