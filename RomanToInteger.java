12a105af720182412d18ed1a3cb9cdf7b0801fa8
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-10-17 22:40:30 +0530

    Updated convert a Roman numeral to an integer problem

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,12 +1,22 @@
+import java.util.*;
+
 public class RomanToInteger {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = Map.of(
-                'I', 1, 'V', 5, 'X', 10,
-                'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+        // Manually initialize the map
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
         int sum = 0;
+
         for (int i = 0; i < s.length(); i++) {
             int val = map.get(s.charAt(i));
+
             if (i + 1 < s.length() && val < map.get(s.charAt(i + 1))) {
                 sum -= val;
             } else {
