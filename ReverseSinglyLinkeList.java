aa27c5cd5d0de0aec929cb27b4780041d991e8bf
SumedhaBedradi <sumedhabhat121@gmail.com>
2025-10-18 18:00:14 +0530

    Reverse singly linked list

---
+++ ReverseSinglyLinkeList.java
@@ -0,0 +1,48 @@
+public class ReverseSinglyLinkeList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+            if (head.next != null) System.out.print(" -> ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        // Reverse the list
+        ReverseSinglyLinkeList reverser = new ReverseSinglyLinkeList();
+        ListNode reversed = reverser.reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(reversed);
+    }
+}
